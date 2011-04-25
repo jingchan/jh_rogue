8781f4ff8adb70acb23b00872d0b15132d26dc9d
Jing Chan <jingchan@gmail.com>
2011-04-25 02:27:54 -0400

    Cleaned up comments and debug printf()s

--- client/src/juggernaut/MainGame.java
+++ client/src/juggernaut/MainGame.java
@@ -1,31 +1,21 @@
 package juggernaut;
  
 import com.jme3.app.SimpleApplication;
-import com.jme3.asset.plugins.ZipLocator;
-import com.jme3.bullet.BulletAppState;
-import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
-import com.jme3.bullet.collision.shapes.CollisionShape;
-import com.jme3.bullet.control.CharacterControl;
-import com.jme3.bullet.control.RigidBodyControl;
-import com.jme3.bullet.util.CollisionShapeFactory;
 
-import com.jme3.input.KeyInput;
+//import com.jme3.input.KeyInput;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.MouseButtonTrigger;
 
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.AnalogListener;
-import com.jme3.input.controls.KeyTrigger;
-import com.jme3.light.AmbientLight;
-import com.jme3.light.DirectionalLight;
+//import com.jme3.input.controls.KeyTrigger;
+//import com.jme3.light.AmbientLight;
+//import com.jme3.light.DirectionalLight;
 
 import com.jme3.math.*;
 import com.jme3.scene.*;
 import com.jme3.scene.shape.*;
-import com.jme3.input.ChaseCamera;
-
 import com.jme3.system.AppSettings;
-
 import com.jme3.material.Material;
 
 import com.jme3.collision.*;
@@ -39,7 +29,6 @@ public class MainGame extends SimpleApplication implements ActionListener {
 	private Node mapRootNode;
 	private Player player;
 	private Geometry playerModel;
-	private ChaseCamera chaseCam;
 	private Geometry mouseIndicator;
 	private boolean mouseActive;
 	public static void main(String[] args) {
@@ -76,7 +65,6 @@ public class MainGame extends SimpleApplication implements ActionListener {
 	
 	@Override
 	public void simpleUpdate( float tpf ) {
-		
 		player.update(tpf);
 		updateCamera();
 		updateDrawEntities();
@@ -85,30 +73,14 @@ public class MainGame extends SimpleApplication implements ActionListener {
 	
 	private void initCamera(){
 		viewPort.setBackgroundColor(new ColorRGBA(172.0f/255.0f,214.0f/255.0f,243.0f/255.0f,1f));
-		flyCam.setEnabled(false);
-//	    flyCam.setMoveSpeed(10);
-//	    flyCam.setUpVector(new Vector3f(0.0f, 0.0f, 1.0f));
-//	    flyCam.setDragToRotate(true);
-//	    cam.lookAtDirection(new Vector3f(0.0f, 1.0f, 0.0f), 
-//	    					new Vector3f(0.0f, 0.0f, 1.0f));
-//	    cam.setLocation(location)
-//	    chaseCam = new ChaseCamera(cam, playerModel, inputManager);
-//	    chaseCam.setUpVector(new Vector3f(0.0f, 0.0f, 1.0f));
-	    
-	    //chaseCam.setLookAtOffset(new Vector3f(0, 0, 4));
-	    
-//        chase_cam.setDragToRotate(false);
-//        chase_cam.setInvertVerticalAxis(true);
-//        chase_cam.setLookAtOffset(new Vector3f(0, 2f, 0));
 
+		// Disable flyCam (instantiated by SimpleApplication)
+		flyCam.setEnabled(false);
+		
+		// Set up camera with default values
 		cam.getLocation().set(0.0f, 0.0f, 0.0f);
-		//cam.getLocation().set(0.0f, 0.0f, 0.0f);
 		cam.getUp().set(0.0f,0.0f,1.0f);
 		cam.getLeft().set(-1.0f,0.0f,0.0f);
-		//cam.setParallelProjection(true);
-		System.out.print(cam.toString());
-		
-
 	}
 	
 	private void initMap(){
@@ -165,8 +137,6 @@ public class MainGame extends SimpleApplication implements ActionListener {
 			if (name.equals("MovePlayer")) {
 				if(keyPressed){
 					mouseActive = true;
-					
-					
 				}
 				else{
 					mouseActive = false;
@@ -176,14 +146,10 @@ public class MainGame extends SimpleApplication implements ActionListener {
 		}
 	};
 
+	@SuppressWarnings("unused")
 	private AnalogListener analogListener = new AnalogListener() {
 		public void onAnalog(String name, float value, float tpf) {
-//			if(name.equals("MovePlayer")){
-////					Vector3f v = player.getLocalTranslation();
-////					player.setLocalTranslation(v.x - value*speed, v.y, v.z);
-//				
-//
-//			}
+			
 		}
 	};
 
@@ -192,50 +158,39 @@ public class MainGame extends SimpleApplication implements ActionListener {
 		Vector3f newCameraPosition = player.position.add(-6.0f, -6.0f, 10.0f);
 		cam.setLocation(newCameraPosition);
 		cam.lookAt(player.position, Vector3f.UNIT_Z);
-	
-//		Vector3f cameraLocation = cam.getLocation();
-//		System.out.printf("CamLocation: %f, %f, %f\n", cameraLocation.x, cameraLocation.y, cameraLocation.z);
-//		System.out.printf("CamTarget: %f, %f, %f\n", player.position.x, player.position.y, player.position.z);
 	}
 	
 	void updateDrawEntities(){
 		playerModel.setLocalTranslation(player.position);
 		Matrix3f rotationMat = new Matrix3f();
 		rotationMat.fromStartEndVectors(Vector3f.UNIT_Y, player.direction.normalize());
-		playerModel.setLocalRotation(rotationMat);
-//		System.out.printf(String.valueOf(player.position.x) + " ");
-//		System.out.printf(String.valueOf(player.position.y) + " ");
-//		System.out.printf(String.valueOf(player.position.z) + "\n");
+		playerModel.setLocalRotation(rotationMat);;
 	}
 
 	void handleInputs(){
 	
 		if(mouseActive){
-			Vector2f mousePosition = inputManager.getCursorPosition();
-			Vector3f mouseOrigin = cam.getWorldCoordinates(mousePosition, 0.0f);
-			Vector3f mouseWorldCoord = cam.getWorldCoordinates(mousePosition, 0.999f);
+			Vector3f mouseOrigin = cam.getLocation();
+			Vector2f mouseScreenCoord = inputManager.getCursorPosition();
+			Vector3f mouseWorldCoord = cam.getWorldCoordinates(mouseScreenCoord, 0.1f);
 			Vector3f mouseDirection = mouseWorldCoord.subtract(mouseOrigin).normalizeLocal();
-			System.out.printf("Cam:\t%f\t%f\t%f\n", cam.getLocation().x, cam.getLocation().y, cam.getLocation().z);
-			System.out.printf("MouseOrigin:\t%f\t%f\t%f\n", mouseOrigin.x, mouseOrigin.y, mouseOrigin.z);
-			System.out.printf("Mouse:\t%f\t%f\t%f\n", mouseWorldCoord.x, mouseWorldCoord.y, mouseWorldCoord.z);
 			
-			//Ray ray = new Ray(cam.getLocation(), mouseWorldCoord);
 			Ray ray = new Ray(mouseOrigin, mouseDirection);
-			System.out.printf("limit:%f\n", ray.limit);
 			CollisionResults collided = new CollisionResults();
 			mapRootNode.collideWith(ray, collided);
 			
+			// NB: This results in false negatives due to edge collision errors.
+			//     Need to be replaced with a more general z=0 plane collision
 			
-			System.out.printf("Collision Size: %d\n", collided.size());
 			if(collided.size()>0){
 				CollisionResult closestCollision = collided.getClosestCollision();
 				mouseIndicator.setLocalTranslation(closestCollision.getContactPoint());
-				//System.out.printf("%f, %f, %f\n", closestCollision.getContactPoint().x, closestCollision.getContactPoint().y, closestCollision.getContactPoint().z);
 				player.direction = closestCollision.getContactPoint().subtract(player.position);
 				player.setState(PlayerState.RUNNING);
 			}
-			else
+			else{
 				player.setState(PlayerState.IDLE);
+			}
 		}
 	}
 }
